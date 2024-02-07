0ba7e4c6026d83a0e80a49d4913249c9c610b2cc
Kelly Silveri <kelly@Kellys-MacBook-Air-20.local>
2024-02-07 10:18:41 -0500

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Kelly Silveri.
+                automatically reload. Kelly Silveri. Hello World.
             </p>
         </div>
     );
