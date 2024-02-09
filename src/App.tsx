3acd7bda2d82e85aa1853068d01d201e6c3985de
Kelly Silveri <ksilveri@udel.edu>
2024-02-09 16:27:03 -0500

    trying to fix image

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                <style>
-                    <div id="FF0000"></div>; UD CISC275 with React Hooks and
-                    TypeScript
-                </style>
+                UD CISC275 with React Hooks and TypeScript
             </header>
             <h1> New Header</h1>
             <img
