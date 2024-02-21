9db5c40cef2bfd7c5af9d8b0f41d3261a9248df6
Kelly Silveri <ksilveri@udel.edu>
2024-02-20 21:29:30 -0500

    comleted task 4 and passed all tests

--- src/functions.ts
+++ src/functions.ts
@@ -12,7 +12,17 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let total = 0;
+    if (first > 0) {
+        total = total + first;
+    }
+    if (second > 0) {
+        total = total + second;
+    }
+    if (third > 0) {
+        total = total + third;
+    }
+    return total;
 }
 
 /**
@@ -20,7 +30,8 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    console.log(message.toUpperCase()) + "!";
+    return message.toUpperCase() + "!";
 }
 
 /**
@@ -28,7 +39,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message.endsWith("?");
 }
 
 /**
@@ -37,5 +48,14 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    if (word === "YES" || word === "yes") {
+        return true;
+    }
+    if (word === "NO" || word === "no") {
+        return false;
+    }
+    return null;
+}
+function toUpperCase(message: string): any {
+    throw new Error("Function not implemented.");
 }
