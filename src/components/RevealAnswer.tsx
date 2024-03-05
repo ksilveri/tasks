fdd7299014611cc51b2775b15210faa6fc99834f
Kelly Silveri <ksilveri@udel.edu>
2024-03-05 17:15:13 -0500

    finish reveal answer

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,19 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    /*** need a boolean to determine whether or not the state is visible
+     * need a button named Reveal Answer that with change the answer from visible to invisiible
+     * when the button is clicked the answer should be visible
+     */
+    const [visible, setVisible] = useState<boolean>(false);
+
+    function changeVisible(): void {
+        setVisible(!visible);
+    }
+    return (
+        <span>
+            <Button onClick={changeVisible}>Reveal Answer</Button>
+            {visible && <div>42</div>}
+        </span>
+    );
 }
