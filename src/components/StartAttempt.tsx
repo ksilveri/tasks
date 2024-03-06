786987e11fdd551b4fb3f1ddf6af91c8adc1f27a
Kelly Silveri <ksilveri@udel.edu>
2024-03-05 23:29:03 -0500

    finish start attempts

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,55 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    /***
+     * number of attempts: 4
+     * boolean for whether quiz is in progress
+     * button labeled Start Quiz (put quiz in progress and decreases attempts by 1)
+     * button labeled Stop Quiz (stops quiz from being in progress)
+     * button named Mulligan that increase attempts by 1
+     * when quiz is not in progress, stop quiz button is disabled
+     * when attempts are zero, the start quiz is disabled
+     */
+    const [attempt, setAttempt] = useState<number>(4);
+    const [progress, setProgress] = useState<boolean>(false);
+
+    function startProgress(): void {
+        setProgress(!progress);
+        decreaseAttempt();
+    }
+
+    function endProgress(): void {
+        setProgress(false);
+    }
+
+    function decreaseAttempt(): void {
+        if (attempt > 0) {
+            setAttempt(attempt - 1);
+        }
+    }
+
+    function increaseAttempt(): void {
+        setAttempt(attempt + 1);
+    }
+
+    return (
+        <div>
+            <p>{attempt}</p>
+            <Button
+                onClick={startProgress}
+                disabled={attempt === 0 || progress}
+            >
+                Start Quiz
+            </Button>
+            ;
+            <Button onClick={endProgress} disabled={!progress}>
+                Stop Quiz
+            </Button>
+            ;
+            <Button onClick={increaseAttempt} disabled={progress}>
+                Mulligan
+            </Button>
+            ;
+        </div>
+    );
 }
