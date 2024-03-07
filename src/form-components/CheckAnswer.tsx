d635bae3dfaea49cd621c422fcab31081ebaa613
Kelly Silveri <ksilveri@udel.edu>
2024-03-07 02:04:20 -0500

    finished check answers

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,39 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
+    /***
+     * make a textbox
+     * user enters a short answer question and display correct or incorrect
+     * component takes in expectedAnswer
+     * state to handle the user's given answer
+     * user answer matches expected, display ✔️
+     * other display ❌
+     */
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setUserAnswer] = useState("");
+    const takeUserAnswer: React.ChangeEventHandler<HTMLInputElement> = (
+        event
+    ) => {
+        setUserAnswer(event.target.value);
+    };
+    function checkAnswer(answer: string): JSX.Element {
+        if (answer === expectedAnswer) {
+            return <span>✔️</span>;
+        } else {
+            return <span>❌</span>;
+        }
+    }
     return (
         <div>
-            <h3>Check Answer</h3>
+            <Form.Group controlId="formCheckAnswer">
+                <Form.Label>Answer:</Form.Label>
+                <Form.Control value={userAnswer} onChange={takeUserAnswer} />
+                {checkAnswer(userAnswer)};
+            </Form.Group>
         </div>
     );
 }
